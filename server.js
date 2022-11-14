514133e37daf2ac5ce0727479b157c2e27d285e9
Yelnur Seitzhanov <yelnur@MacBook-Pro-Yelnur-S.local>
2022-11-14 20:43:29 +0600

    fix cors

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const bodyParser = require('body-parser')
 const logger = require('morgan')
 const path = require('path')
 const passport = require('passport')
+const cors = require("cors");
 require('./config/db').database()
 
 require('./config/passport')(passport)
 
 const app = express()
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3001)
 
 app.use(express.static(path.join(__dirname, './public'), { maxAge: 1 }))
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(logger('dev'))
+app.use(cors());
 
 app.use('/api', require('./routes'))
 
